16493e9cb1859df38dbaaa4b5ba0c6c2a9abdb3f
mhgolestan <golestan1369@gmail.com>
2022-05-04 14:05:32 +0200

    4.1 Blog list, step1 and 4.1 Blog list, step2

---
+++ controllers/routes.js
@@ -0,0 +1,69 @@
+const Model = require("../models/model");
+
+const express = require("express");
+const blogRouter = express.Router();
+
+//Post Method
+blogRouter.post("/", async (req, res) => {
+  const data = new Model({
+    title: req.body.title,
+    author: req.body.author,
+    url: req.body.url,
+    likes: req.body.likes,
+  });
+
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Get all Method
+blogRouter.get("/", async (req, res) => {
+  try {
+    const data = await Model.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Get by ID Method
+blogRouter.get("/:id", async (req, res) => {
+  try {
+    const data = await Model.findById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Update by ID Method
+blogRouter.patch("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
+
+    const result = await Model.findByIdAndUpdate(id, updatedData, options);
+
+    res.send(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Delete by ID Method
+blogRouter.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Model.findByIdAndDelete(id);
+    res.send(`Document with ${data.name} has been deleted..`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = blogRouter;
